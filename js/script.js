9cdef1e6f3b6e59e387008c6ba849a026d71fe40
Daniel Lim <dlim7@hawk.iit.edu>
2022-11-08 11:11:51 -0600

    Completed lab

--- js/script.js
+++ js/script.js
@@ -1,11 +1,17 @@
 function init(){
-//add your javascrip between these two lines of code
- 
+  
+  var input = document.getElementById('entryinput')
+  var pressButton = document.getElementById('entrybutton')
+  var output = document.getElementById('textoutput')
+  
+  function alertFunc() {
+    //sends the alert
+    alert('Daniel Lim: ' + input.value)
+    //changes h2
+    output.innerHTML = input.value;
+  }
+  
+  pressButton.addEventListener('click', alertFunc);
 
-
-
-
-
-
-
+}
 window.addEventListener('load', init);
